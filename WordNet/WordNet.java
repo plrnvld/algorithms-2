97c3575a4004cbf49975694aaca70b847566c942
plrnvld <paulrijneveld@gmail.com>
2024-01-10 21:26:56 +0100

    Add requested class starting points

---
+++ WordNet/WordNet.java
@@ -0,0 +1,48 @@
+import java.lang.*;
+import java.util.*;
+
+public class WordNet {
+
+    // constructor takes the name of the two input files
+    public WordNet(String synsets, String hypernyms) {
+        if (synsets == null || hypernyms == null)
+            throw new IllegalArgumentException();
+
+    }
+
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+        return null;
+    }
+
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        if (word == null)
+            throw new IllegalArgumentException();
+
+        return false;
+    }
+
+    // distance between nounA and nounB (defined below)
+    public int distance(String nounA, String nounB) {
+        if (nounA == null || nounB == null)
+            throw new IllegalArgumentException();
+
+        return -1;
+    }
+
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA
+    // and nounB
+    // in a shortest ancestral path (defined below)
+    public String sap(String nounA, String nounB) {
+        if (nounA == null || nounB == null)
+            throw new IllegalArgumentException();
+
+        return null;
+    }
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+        System.out.println("Let's work");
+    }
+}
