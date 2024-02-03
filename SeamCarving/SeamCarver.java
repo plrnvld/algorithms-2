a306c3867dca142f38722f9deaaec635c4bf7eb5
plrnvld <paulrijneveld@gmail.com>
2024-02-03 14:19:42 +0100

    Add exceptions and link library

--- SeamCarving/SeamCarver.java
+++ SeamCarving/SeamCarver.java
@@ -2,28 +2,36 @@ import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
 
+    private Picture pic;
+
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        if (picture == null)
+            throw new IllegalArgumentException();
 
+        pic = picture;
     }
 
     // current picture
     public Picture picture() {
-        return null;
+        return pic;
     }
 
     // width of current picture
     public int width() {
-        return -1;
+        return pic.width();
     }
 
     // height of current picture
     public int height() {
-        return -1;
+        return pic.height();
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
+        if (x < 0 || x >= width() || y < 0 || y >= height())
+            throw new IllegalArgumentException();
+
         return -1.0;
     }
 
@@ -39,10 +47,14 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
+        if (seam == null)
+            throw new IllegalArgumentException();
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        if (seam == null)
+            throw new IllegalArgumentException();
     }
 
     // unit testing (optional)
