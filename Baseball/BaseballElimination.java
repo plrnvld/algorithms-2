4deb5aaf3208a5c0eec2121738a160755fa0c78d
plrnvld <paulrijneveld@gmail.com>
2024-04-21 22:08:31 +0200

    Prepare for reading file

--- Baseball/BaseballElimination.java
+++ Baseball/BaseballElimination.java
@@ -1,49 +1,72 @@
-import edu.princeton.cs.algs4.DijkstraSP;
+import edu.princeton.cs.algs4.ST;
 
 class BaseballElimination {
 
+    private ST<String, Integer> teamsTable;
+    private int w[];
+    private int l[];
+    private int r[];
+    private int g[][];
+
+
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
+        teamsTable = new ST<>();
+
+        // ############### Read teams, add indexes
+    }
+
+    private int teamIndex(String team) {
+        if (teamsTable.contains(team))
+            return teamsTable.get(team);
+
+        throw new IllegalArgumentException("Invalid team: " + team);
     }
 
     // number of teams
     public int numberOfTeams() {
-        return -1;
+        return teamsTable.size();
     }
 
     // all teams
     public Iterable<String> teams() {
-        return null;
+        return teamsTable.keys();
     }
 
     // number of wins for given team
     public int wins(String team) {
-        return -1;
+        return w[teamIndex(team)];
     }
 
     // number of losses for given team
     public int losses(String team) {
-        return -1;
+        return l[teamIndex(team)];
     }
 
     // number of remaining games for given team
     public int remaining(String team) {
-        return -1;
+        return l[teamIndex(team)];
     }
 
     // number of remaining games between team1 and team2
     public int against(String team1, String team2) {
-        return -1;
+        int index1 = teamIndex(team1);
+        int index2 = teamIndex(team2);
+        return g[index1][index2];
     }
 
     // is given team eliminated?
     public boolean isEliminated(String team) {
+        int index = teamIndex(team);
+
         return false;
     }
 
     // subset R of teams that eliminates given team; null
     // if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
+        int index = teamIndex(team);
+        
         return null;
     }
 
