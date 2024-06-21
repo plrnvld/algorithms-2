9d95b5755ac269f22f95c0e8d2364e482fc3f467
plrnvld <paulrijneveld@gmail.com>
2024-06-21 10:12:38 +0200

    Add TST with words + scoring mechanism

--- Boggle/BoggleSolver.java
+++ Boggle/BoggleSolver.java
@@ -1,13 +1,21 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.TST;
 
-public class BoggleSolver
-{
-    // Initializes the data structure using the given array of strings as the dictionary.
-    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+public class BoggleSolver {
+    TST<Integer> wordsInDictionary;
+
+    // Initializes the data structure using the given array of strings as the
+    // dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase
+    // letters A through Z.)
     public BoggleSolver(String[] dictionary) {
+        wordsInDictionary = new TST<>();
 
+        for (String word : dictionary) {
+            wordsInDictionary.put(word, unsafeScoreOf(word));
+        }
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
@@ -15,10 +23,38 @@ public class BoggleSolver
         return null;
     }
 
-    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // Returns the score of the given word if it is in the dictionary, zero
+    // otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
-        return -1;
+        Integer scoreValue = wordsInDictionary.get(word);
+
+        if (scoreValue == null)
+            return 0;
+
+        return scoreValue;
+    }
+
+    // Give the score for a word, but don't check if it's in the dictionary
+    private int unsafeScoreOf(String word) {
+        int numChars = word.length();
+        switch (numChars) {
+            case 0:
+            case 1:
+            case 2:
+                return 0;
+            case 3:
+            case 4:
+                return 1;
+            case 5:
+                return 2;
+            case 6:
+                return 3;
+            case 7:
+                return 5;
+            default:
+                return 11;
+        }
     }
 
     public static void main(String[] args) {
