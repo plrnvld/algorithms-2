aa34c0d7d0c0e533deab729c1fb4e195d4a857b2
plrnvld <paulrijneveld@gmail.com>
2024-07-12 11:13:04 +0200

    Implement MoveToFront main method

--- Burrows/MoveToFront.java
+++ Burrows/MoveToFront.java
@@ -1,11 +1,12 @@
 public class MoveToFront {
-
-    // apply move-to-front encoding, reading from standard input and writing to standard output
+    // apply move-to-front encoding, reading from standard input and writing to
+    // standard output
     public static void encode() {
 
     }
 
-    // apply move-to-front decoding, reading from standard input and writing to standard output
+    // apply move-to-front decoding, reading from standard input and writing to
+    // standard output
     public static void decode() {
 
     }
@@ -13,7 +14,12 @@ public class MoveToFront {
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
     public static void main(String[] args) {
-        System.out.println("MoveToFront");
-    }
+        if (args.length != 1 || !(args[0].equals("+") || args[0].equals("-")))
+            throw new IllegalArgumentException("One argument required: + for encoding or - for decoding");
 
+        if (args[0].equals("+"))
+            encode();
+        else
+            decode();
+    }
 }
