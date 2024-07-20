45bf430f12b33c842a2ab8f9257396eddbc81167
plrnvld <paulrijneveld@gmail.com>
2024-07-20 21:07:50 +0200

    Fix CircularSuffixArray, score 100%

--- Burrows/CircularSuffixArray.java
+++ Burrows/CircularSuffixArray.java
@@ -1,52 +1,51 @@
-import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.TrieSET;
-import edu.princeton.cs.algs4.TrieST;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class CircularSuffixArray {
     private final String s;
-    private final int[] indices;
+    private final Integer[] indices;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
         if (s == null)
             throw new IllegalArgumentException("Null value not allowed");
         this.s = s;
-        this.indices = new int[length()];
+        this.indices = new Integer[length()];
 
-        TrieST<Integer> trie = new TrieST<>();
+        for (var i = 0; i < indices.length; i++) {
+            indices[i] = i;
+        }
 
-        int startPos = 0;
-        for (var i = 0; i < length(); i++) {
-            var suffixQueue = new Queue<>();
-                
-            for (var n = 0; n < Math.min(20, length()); n++) { // ########### Hack in performance optimization, this breaks correctness!
-                var pos = (startPos + n) % length();
+        Arrays.sort(indices, new SuffixComparator(s));
+    }
 
-                suffixQueue.enqueue(s.charAt(pos));
+    private class SuffixComparator implements Comparator<Integer> {
+
+        private String s;
+
+        public SuffixComparator(String s) {
+            this.s = s;
+        }
+
+        @Override
+        public int compare(Integer shiftLeft1, Integer shiftLeft2) {
+            var shift1 = (int) shiftLeft1;
+            var shift2 = (int) shiftLeft2;
+
+            if (shift1 == shift2)
+                return 0;
+
+            for (var i = 0; i < s.length(); i++) {
+                int char1 = s.charAt((shift1 + i) % s.length());
+                int char2 = s.charAt((shift2 + i) % s.length());
+
+                if (char1 != char2)
+                    return char1 - char2;
             }
 
-            var suffixArray = new char[suffixQueue.size()];
-            int suffixPos = 0;
-            for (var c : suffixQueue) {
-                suffixArray[suffixPos++] = (char) c;
-            }
-
-            trie.put(new String(suffixArray), i);
-
-            startPos++;
+            return 0;
         }
 
-        TrieSET set = new TrieSET();
-        for (var key : trie.keys()) {
-            set.add(key);
-        }
-
-        int n = 0;
-        for (var item: set) {
-            indices[n] = trie.get(item);
-
-            n++;
-        }
     }
 
     // length of s
